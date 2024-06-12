242ad77850ecca9c9e710651928d73c90c3fde0c
Eetutsu <122541775+Eetutsu@users.noreply.github.com>
2024-06-12 12:36:35 +0300

    B4 finished

--- __main__.py
+++ __main__.py
@@ -4,8 +4,10 @@ import test_matrices
 
 for matrix in test_matrices.matrices.keys():
     lower_bound = lb.rank_based_lower_bound(test_matrices.matrices[matrix])
-    print(f"PSD Rank Lower bound for {matrix}: {lower_bound}")
-
-for matrix in test_matrices.matrices.keys():
+    print(f"PSD Rank Lower bound for {matrix}: {lower_bound} using rank_based")
+    lower_bound = lb.B4(test_matrices.matrices[matrix])
+    print(f"PSD Rank Lower bound for {matrix}: {lower_bound} B4")
     upper_bound = ub.mindim_upper_bound(test_matrices.matrices[matrix])
     print(f"PSD Rank Upper bound for {matrix}: {upper_bound}")
+
+    
--- lower_bound.py
+++ lower_bound.py
@@ -6,3 +6,21 @@ import numpy as np
 def rank_based_lower_bound(M):
     lower_bound = (1/2)*np.sqrt(1+8*matrix_rank(M))-(1/2)
     return lower_bound
+
+def B4(M):
+    epsilon = 0.0001
+    rowsum = 0
+    for row in M:
+        for i in row:
+            rowsum += i
+        if 1-rowsum>=epsilon:
+            break
+        else: rowsum = 0
+    if rowsum != 0:
+        return "Not a row stochastic matrix"
+    else:
+        sum = 0.0
+        maxes = [max(row) for row in M]
+        for i in maxes:
+            sum += i
+        return sum
