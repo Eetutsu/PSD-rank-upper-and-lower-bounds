16653f2e5a18cb6bc2bb0c991684210eb4525ea1
Eetutsu <122541775+Eetutsu@users.noreply.github.com>
2024-06-13 10:59:00 +0300

    Added "is_accurate = False" to haramard sqrt

--- upper_bound.py
+++ upper_bound.py
@@ -8,24 +8,25 @@ def mindim_upper_bound(M):
     return min(dims)
 
 
-def hadamard_sqrt_upper_bound(M):
+def hadamard_sqrt_upper_bound(M, is_accurate = False):
     """"POSITIVE SEMIDEFINITE RANK" https://arxiv.org/pdf/1407.4095 Corollary 5.3 Page 18"""
-    row = 0
-    col = 0
-    sqrt_ranks = []
-    for k in range(1000):
-        m = np.array(M)
-        for i in m:
-            for j in i:
-                rng = np.random.randint(0,2)
-                if rng == 0:
-                    m[row,col] = -np.sqrt(j)
-                    col += 1
-                else: 
-                    m[row,col] = np.sqrt(j)
-                    col += 1
-            col = 0
-            row += 1
-        sqrt_ranks.append(matrix_rank(m))
-        col = row = 0
-    return min(sqrt_ranks)
+    if not is_accurate:
+        row = 0
+        col = 0
+        sqrt_ranks = []
+        for k in range(1000):
+            m = np.array(M)
+            for i in m:
+                for j in i:
+                    rng = np.random.randint(0,2)
+                    if rng == 0:
+                        m[row,col] = -np.sqrt(j)
+                        col += 1
+                    else: 
+                        m[row,col] = np.sqrt(j)
+                        col += 1
+                col = 0
+                row += 1
+            sqrt_ranks.append(matrix_rank(m))
+            col = row = 0
+        return min(sqrt_ranks)
